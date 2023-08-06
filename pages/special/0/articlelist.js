eb22c79364bb0dcebb5bd45512a7fcc461af312b
5h9igzqanx <zhang_qixuan59@163.com>
2023-08-06 18:02:48 +0800

    master

--- pages/special/0/articlelist.js
+++ pages/special/0/articlelist.js
@@ -1,5 +1,5 @@
-var articleList = [
-    {id:"202212250001",title:"核聚变(仅作测试)",href:"22/202212250001"},
+const articleList = [
+    {id:"22/202212250001",title:"核聚变(仅作测试)",href:"22/202212250001"},
     {id:"",title:"",href:""},
     {id:"",title:"",href:""},
 ]// 列表部分
